6d9bf02721047f87d5424fe703d9cbc9066c077a
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-10-17 14:44:05 +0300

    Add files via upload

--- lesson13/1/script.js
+++ lesson13/1/script.js
@@ -10,6 +10,17 @@ bb[0].addEventListener('click',function(){
     document.querySelector('.box7').classList.add('bg-gray','bor');
     document.querySelector('.box8').classList.add('bg-violet','bor');
     document.querySelector('.box9').classList.add('bg-coral','bor');
+    document.querySelector('.box1').classList.remove('b1-img');
+    document.querySelector('.box2').classList.remove('b2-img');
+    document.querySelector('.box3').classList.remove('b3-img');
+    document.querySelector('.box4').classList.remove('b4-img');
+    document.querySelector('.box5').classList.remove('b5-img');
+    document.querySelector('.box6').classList.remove('b6-img');
+    document.querySelector('.box7').classList.remove('b7-img');
+    document.querySelector('.box8').classList.remove('b8-img');
+    document.querySelector('.box9').classList.remove('b9-img');
+    document.body.style.background = 'White';
+
 })
 
 let bc = document.getElementsByClassName('box-con');
@@ -22,15 +33,15 @@ function addColor(event){
 }
 
 bb[1].addEventListener('click',function(){
-    document.querySelector('.box1').classList.remove('bg-red','bor');
-    document.querySelector('.box2').classList.remove('bg-green','bor');
-    document.querySelector('.box3').classList.remove('bg-yellow','bor');
-    document.querySelector('.box4').classList.remove('bg-brown','bor');
-    document.querySelector('.box5').classList.remove('bg-orange','bor');
-    document.querySelector('.box6').classList.remove('bg-blue','bor');
-    document.querySelector('.box7').classList.remove('bg-gray','bor');
-    document.querySelector('.box8').classList.remove('bg-violet','bor');
-    document.querySelector('.box9').classList.remove('bg-coral','bor');
+    document.querySelector('.box1').classList.remove('bg-red');
+    document.querySelector('.box2').classList.remove('bg-green');
+    document.querySelector('.box3').classList.remove('bg-yellow');
+    document.querySelector('.box4').classList.remove('bg-brown');
+    document.querySelector('.box5').classList.remove('bg-orange');
+    document.querySelector('.box6').classList.remove('bg-blue');
+    document.querySelector('.box7').classList.remove('bg-gray');
+    document.querySelector('.box8').classList.remove('bg-violet');
+    document.querySelector('.box9').classList.remove('bg-coral');
     document.querySelector('.box1').classList.add('b1-img');
     document.querySelector('.box2').classList.add('b2-img');
     document.querySelector('.box3').classList.add('b3-img');
@@ -40,4 +51,5 @@ bb[1].addEventListener('click',function(){
     document.querySelector('.box7').classList.add('b7-img');
     document.querySelector('.box8').classList.add('b8-img');
     document.querySelector('.box9').classList.add('b9-img');
+    document.body.style.background = 'White';
 })
